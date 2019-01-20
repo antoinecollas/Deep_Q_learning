ee96c4a888108bf6d4d2e234c564a99345d6c978
antoinecollas <antoine.collas@etu.utc.fr>
2019-01-20 21:50:59 +0100

    bug fix

--- examples/train_pong.py
+++ examples/train_pong.py
@@ -15,7 +15,7 @@ train_deepq(
     Q_network=Q_network,
     input_as_images=True,
     preprocess_fn=preprocess,
-    replay_start_size=5*10e5,
-    replay_memory_size=5*10e5,
+    replay_start_size=5*int(10e5),
+    replay_memory_size=5*int(10e5),
     tensorboard_freq=5,
     )
